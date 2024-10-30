e2c59601dca3c78152315f2585fb866d481f4f54
TecnomaLaser <adrian.tripp1@gmail.com>
2024-10-30 20:06:13 +0100

    indent

--- structure_generation.py
+++ structure_generation.py
@@ -327,11 +327,11 @@ def combine_screening_results(dir: str, prefixes: list, scores: list, weights: l
 
     # recalculate composite score over all screening runs
     poses.calculate_composite_score(
-                name="design_composite_score",
-                scoreterms=["esm_plddt", "esm_tm_TM_score_ref", "esm_catres_bb_rmsd", "esm_catres_heavy_rmsd", "esm_lig_contacts", "esm_ligand_clashes", "esm_rog_data"],
-                weights=[-1, -1, 4, 3, -0.5, 0.5, 1],
-                plot=True
-            )
+        name="design_composite_score",
+        scoreterms=["esm_plddt", "esm_tm_TM_score_ref", "esm_catres_bb_rmsd", "esm_catres_heavy_rmsd", "esm_lig_contacts", "esm_ligand_clashes", "esm_rog_data"],
+        weights=[-1, -1, 4, 3, -0.5, 0.5, 1],
+        plot=True
+    )
 
     # convert columns to residues (else, pymol script writer and refinement crash)
     for residue_col in residue_cols:
