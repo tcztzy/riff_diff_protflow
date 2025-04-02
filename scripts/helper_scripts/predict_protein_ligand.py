ea344dc017741af61e32c22d15d912ac71258947
mabr3112 <markus.braun95@gmail.com>
2025-04-02 23:15:24 +0200

    minor changes in protein ligand prediction script

--- scripts/helper_scripts/predict_protein_ligand.py
+++ scripts/helper_scripts/predict_protein_ligand.py
@@ -12,64 +12,9 @@ import argparse
 # dependencies
 import protflow
 import numpy as np
-from rdkit import Chem
-from rdkit.Chem import rdFMCS
-from Bio.PDB import PDBParser, Select
-from Bio.PDB.Polypeptide import is_aa
 from protflow.tools.alphafold3 import AlphaFold3
 
-# Extract ligand coordinates from PDB file directly
-def extract_ligand_coords(pdb_file, ligand_resname, chain_id, res_id):
-    parser = PDBParser(QUIET=True)
-    structure = parser.get_structure('struct', pdb_file)
-
-    coords = []
-    atom_names = []
-
-    for model in structure:
-        for chain in model:
-            if chain.id == chain_id:
-                for residue in chain:
-                    if residue.get_resname() == ligand_resname and residue.id[1] == res_id:
-                        for atom in residue:
-                            coords.append(atom.get_coord())
-                            atom_names.append(atom.element)
-    return atom_names, coords
-
-## Create RDKit molecule from coordinates and atom types (without bonds)
-#def create_rdkit_mol(atom_names, coords):
-#    mol = Chem.RWMol()
-#    conformer = Chem.Conformer(len(atom_names))
-#
-#    periodic_table = Chem.GetPeriodicTable()
-#
-#    for i, (element, coord) in enumerate(zip(atom_names, coords)):
-#        atomic_num = periodic_table.GetAtomicNumber(element)
-#        atom = Chem.Atom(atomic_num)
-#        mol.AddAtom(atom)
-#        conformer.SetAtomPosition(i, coord)
-#
-#    mol.AddConformer(conformer)
-#
-#    # Generate bonds based on distances
-#    mol = Chem.rdmolops.AssignBondOrdersFromTemplate(Chem.AddHs(mol), mol)
-#    mol = Chem.RemoveHs(mol)
-#
-#    return mol
-
-
 # Get matching between two RDKit molecules using Maximum Common Substructure
-def get_matching(mol1, mol2):
-    res = rdFMCS.FindMCS([mol1, mol2])
-    patt = Chem.rdmolfiles.MolFromSmarts(res.smartsString)
-
-    match1 = mol1.GetSubstructMatch(patt)
-    match2 = mol2.GetSubstructMatch(patt)
-
-    mapping = dict(zip(match1, match2))
-
-    return mapping
-
 def main(args):
     '''does stuff'''
     # create output
@@ -85,7 +30,7 @@ def main(args):
             logging.StreamHandler()  # Log to console
         ]
     )
-    logging.info(f"Running predict_protein_ligand.py on {args.input_dir} with ligand from {args.ligand_file}")
+    logging.info(f"Running predict_protein_ligand.py on {args.input_dir} with ligand from {args.ligand_dir}")
 
     # parse jobstarter
     if args.jobstarter == "sbatch":
@@ -98,34 +43,35 @@ def main(args):
     # setup af3 runner
     af3 = AlphaFold3(jobstarter=jst)
 
-    # load poses from DataFrame.
-    proteins = protflow.poses.load_poses(poses_path=args.input_poses)
-    logging.info(f"Loaded {len(proteins)} poses from input {args.poses_path}.")
-
     # check if all ligand columns are present in poses_df
     ligands = glob.glob(os.path.join(args.ligand_dir, "*.json"))
 
     # if input poses are in .pdb format, convert to .fa files first.
-    if proteins.poses_list()[0].endswith(".pdb"):
-        proteins.convert_pdb_to_fasta("pdbs_to_fasta", update_poses=True)
+    #if proteins.poses_list()[0].endswith(".pdb"):
+    #    proteins.convert_pdb_to_fasta("pdbs_to_fasta", update_poses=True)
 
     # loop over ligands
-    for ligand in ligands:
+    for i, ligand in enumerate(ligands):
         # first load in the ligand
         logging.info(f"Reading Ligand from {ligand}")
         with open(ligand, 'r', encoding="UTF-8") as f:
             ligand_dict = json.load(f)
-        
+
+        # load poses from DataFrame.
+        batch = protflow.poses.Poses(poses=args.input_dir, glob_suffix="*.pdb", work_dir=os.path.join(args.output_dir, f"batch_{i+1}"))
+        logging.info(f"Loaded {len(batch)} poses from input {args.input_dir}.")
+
         # run af3 on ligand
         logging.info(f"Predicting structure ligand from {ligand}")
         af3.run(
-            poses=proteins,
+            poses=batch,
             prefix=ligand,
-            nstruct=5,
+            nstruct=15,
             additional_entities=ligand_dict, # assumes file {ligand} points to correctly formatted ligand
             options="--flash_attention_implementation xla --cuda_compute_7x 1",
             single_sequence_mode=False,
-            user_ccd=args.custom_ccd_dir
+            user_ccd=args.custom_ccd_dir,
+            return_top_n_models=15
         )
 
     logging.info(f"Prediction of {len(args.ligand_cols)} complex structures completed")
