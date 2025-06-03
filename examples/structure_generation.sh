7652f5543712a9d0ea6d993f200370056a9a055c
Adrian Tripp <adrian.tripp1@gmail.com>
2025-06-03 23:50:30 +0200

    Update structure_generation.sh

--- examples/structure_generation.sh
+++ examples/structure_generation.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-# Example command to generate an artificial motif library.
-# In this case, we directly use the .pdb file of RA95.5-8F as input.
+# Example command to generate de novo enzymes from a motif library.
 # Make sure you run this with your python environment that has protflow installed.
 
 python ../structure_generation.py --riff_diff_dir ../ --working_dir outputs --screen_input_json outputs/selected_paths.json --attnpacker_repack
